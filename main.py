1a189d6fcd2f5138f1a3707b7efeb271f298b067
Amir <amircan206@gmail.com>
2025-08-28 08:13:43 -0400

    imported os, imported cryptography

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import os
 import user
-from user import User
+from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
 
 
 def microservice():
     amir = user.User("amir", 24, "Green")
-    print(amir.age)
+    print(amir.name)
 
 
 
