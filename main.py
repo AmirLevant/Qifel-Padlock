48f6b9f1c8bb9e333c090a476803136e574c05fc
Amir <amircan206@gmail.com>
2025-08-28 09:01:06 -0400

    Testing AES-GES Encryption

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 import os
 import user
-from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
-
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 def microservice():
     amir = user.User("amir", 24, "Green")
-    print(amir.name)
+    return amir.name
+
+
+# Testing AES-GCM which is block cipher using Galois Counter Mode
+def encryption_test():
+    initialdata = microservice() # Data is amir
+    final_data = b""+initialdata
+
+    key = AESGCM.generate_key(bit_length=128) 
+    aesgcm = AESGCM(key)
+    nonce = os.urandom(12)
+    cipher_text = aesgcm.encrypt(nonce,final_data,None)
+
+
+    # decrypting
+    aesgcm.decrypt(nonce,cipher_text,None)
+
+
 
 
 
 
 
 def main():
-    microservice()
+    #encryption_test
+    initialdata = microservice() # Data is amir
+    final_data = b""+initialdata
+
+    print(final_data)
     
 
 
